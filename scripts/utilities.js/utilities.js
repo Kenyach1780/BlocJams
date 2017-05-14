a96b798e709ed0a9245dfd29f95f00510b40dbc7
Kenya Harvey <KenyaCH1780@users.noreply.github.com>
2017-05-14 11:30:13 -0500

    Assignment 9 events: updated

--- scripts/utilities.js/utilities.js
+++
@@ -1,7 +0,0 @@
-var pointsArray = document.getElementsByClassName('point');
-
-function forEach() {
-    for (var i = 0; i < pointsArray.length; i++) {
-        console.log(pointsArray[i]);
-    }
-}
